cb301d83f0e8451671eb70bc8ec941af1e992024
denisbyte <denisafoh22@gmail.com>
2025-11-04 16:10:10 +0100

    Ajout du fichier downalder pour le téléchargement de l'image docker

---
+++ downalder.py
@@ -0,0 +1,29 @@
+import docker
+import logging
+
+#Permet de faire la communication avec le serveur docker installé sur la machine hote
+client = docker.from_env()
+
+#Fonction de Connexion
+def docker_login(username = None, password = None):
+    if username and password:
+        client.login(username = username, password=password)
+        logging.info("Authentification avec succès sur DockerHub")
+
+    else:
+        logging.info("Aucune authentification requise(image public)")
+
+
+#Fonction de récupération d'image
+
+def pull_image(image_name):
+    logging.info(f"Télechargement de l'image {image_name} depuis Docker Hub")
+    #Téléchargement de l'image avec client Docker
+    image = client.images.pull(image_name)
+
+    #Affichage des informations de tag de l'image téléchargé
+    logging.info(f"Télechargement terminé :{image.tags}")
+    
+    return image
+
+
